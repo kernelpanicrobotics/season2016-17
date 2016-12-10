12d4a98c1ddb532b9a9a41e0e8ad7099c36d2665
kernel panic <ftckernalpanic@gmail.com>
2016-12-10 13:39:30 -0500

    drive to white line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KernelPanicAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KernelPanicAutonomous.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 
 
 /**
@@ -18,8 +19,14 @@ public class KernelPanicAutonomous extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         int currentHeading;
         int nextHeading;
+        //public static final int COLOR_SENSOR_BOTTOM_ADDRESS = 0x3c>>1;
+        //public ColorSensor colorBottom = null;
+        boolean continueForward = true;
+
 
         robot.init(hardwareMap);
+
+
         //Clean this up because of drive class
         DcMotor[] leftMotors = new DcMotor[]{ robot.leftMotorFront, robot.leftMotorBack };
         DcMotor[] rightMotors = new DcMotor[]{ robot.rightMotorFront, robot.rightMotorBack};
@@ -28,38 +35,62 @@ public class KernelPanicAutonomous extends LinearOpMode {
         waitForStart();
 
         // Move forward some
-        myDrive.moveForward(10, 0.3);
+        myDrive.moveForward(12, 0.3);
         while (myDrive.motorsRunning() == true) {
             myDrive.update();
         }
 
         //Try to turn 45 degrees
         currentHeading = robot.gyro.getHeading();
-        nextHeading = newHeading(currentHeading, 60);
-        myDrive.driveMove(0,0.25);
+        nextHeading = newHeading(currentHeading, 45);
+        myDrive.driveMove(0, 0.25);
         while(robot.gyro.getHeading() <= nextHeading) {
             //Kill some time
             telemetry.addData("heading", robot.gyro.getHeading());
             telemetry.update();
         }
 
-        myDrive.moveForward(30, 0.3);
+        myDrive.moveForward(42, 0.3);
         while (myDrive.motorsRunning() == true) {
             myDrive.update();
         }
 
         currentHeading = robot.gyro.getHeading();
-        nextHeading = newHeading(currentHeading,-30);
-        myDrive.driveMove(0,0.25);
-        while(robot.gyro.getHeading() <= nextHeading) {
+        nextHeading = newHeading(currentHeading, -45);
+        myDrive.driveMove(0, -0.25);
+        while(robot.gyro.getHeading() >= nextHeading) {
             //Kill some time
             telemetry.addData("heading", robot.gyro.getHeading());
             telemetry.update();
         }
 
+        myDrive.moveForward(10, 0.3);
+        while (myDrive.motorsRunning() == true) {
+            myDrive.update();
+        }
 
-        myDrive.allStop();
+       /* myDrive.moveForward(30, 0.1);
+        while ((myDrive.motorsRunning() == true)&&
+                (continueForward == true)){
+            myDrive.update();
 
+
+        }*/
+        long mytime = System.currentTimeMillis();
+        long loopingtime = 0;
+        myDrive.driveMove(.1,0);
+        boolean keepmoving = true;
+        while (keepmoving == true) {
+              if (((robot.colorBottom.red() > 20) &&
+                    (robot.colorBottom.blue() > 20) &&
+                    (robot.colorBottom.green() > 20)) ||
+                    (loopingtime > 5000)){
+                myDrive.allStop();
+                keepmoving = false;
+            }
+            loopingtime = System.currentTimeMillis() - mytime;
+
+        }
 
 
         while(opModeIsActive()) {
